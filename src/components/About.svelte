6efc143863388aef7371096597a09ce91254ab18
Chrystian Lozano <chrystianlozano@icloud.com>
2020-04-23 19:58:23 -0500

    Condicionales

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,8 +3,15 @@
 let someText = `Fronted Developer at chrystian.dev`
 let count = 0
 
+let style = { darkMode: false }
+
 function handleClick(){
     count++
+}
+
+function toggle(){
+    style.darkMode = !style.darkMode
+    window.document.body.classList.toggle("dark-mode")
 }
 
 </script>
@@ -15,8 +22,16 @@ p{
 </style>
 
 <div class="About">
+
+{#if !style.darkMode}
+<p>{someText}</p>
+{:else}
 <p>
-    {someText}
+<span>Hola Dark Mode</span>
 </p>
+{/if}
+
+
 <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+<button on:click={toggle}>DarkMode</button>
 </div>
